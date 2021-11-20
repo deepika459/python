0091c16f1d435d227f6ec9d2cc144aa630967b91
Deepika <kdeepika459@gmail.com>
2021-11-20 19:09:49 +0530

    second commit

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+           {
+           'id':'1',
+           'title':'Ecommerce Website',
+           'description': 'Fully functional ecommerce website'
+           },
+           {
+           'id':'2',
+           'title': "Portfolio Website",
+           'description': 'this was a project where i built out my portfol'
+
+           },
+           {
+           'id':'3',
+           'title':'Social networl',
+           'description': 'Awsome open source project iam still woring'
+
+           },
+
+]
 def projects(request):
     page = 'projects'
     number = 9
-    context = {'page': page, 'number': number}
+    context = {'page': page, 'number': number, 'projects': projectsList}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectobj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectobj = i
+    return render(request, 'projects/single-project.html', { 'project': projectobj})
 
 # Create your views here.
